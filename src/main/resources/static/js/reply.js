540130e57b92258a607a678b0e3d2fb442a3dbc3
chae1-1111 <dla4212@naver.com>
2023-01-12 16:30:15 +0900

    complete to make a Board

--- src/main/resources/static/js/reply.js
+++ src/main/resources/static/js/reply.js
@@ -1,6 +1,32 @@
-async function get1(bno) {
+const getList = async (bno, page, size, goLast) => {
 
-    const result = await axios.get(`/replies/list/${bno}`);
+    const result = await axios.get(`/replies/list/${bno}`, {params: {page, size}});
 
-    console.log(result);
+    if(goLast) {
+        const total = result.data.total;
+        const lastPage = parseInt(Math.ceil(total/size));
+        return getList(bno, lastPage, size);
+    }
+
+    return result.data;
+}
+
+const addReply = async (replyObj) => {
+    const response = await axios.post(`/replies/`, replyObj);
+    return response.data;
+}
+
+const getReply = async (rno) => {
+    const response = await axios.get(`/replies/${rno}`);
+    return response.data;
+}
+
+const modifyReply = async (replyObj) => {
+    const response = await axios.put(`/replies/${replyObj.rno}`, replyObj);
+    return response.data;
+}
+
+const removeReply = async (rno) => {
+    const response = await axios.delete(`/replies/${rno}`);
+    return response.data;
 }
